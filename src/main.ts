91e75d2b3cd54bf07f128212da7626fdc9b9b4f3
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-20 15:33:17 +0900

    nest.js 롤 연습 프로그램

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    // Dto를 사용하기 위해서 추가한 로직
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  await app.listen(3000);
 }
+
 bootstrap();
